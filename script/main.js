9ad0a438e395f885406a70bdfa368903ae08b1dd
Giorgi Chkhikvadze <giorgichxikvadze972@gmail.com>
2022-08-30 01:32:18 +0400

    fetched data for teams, positions, brands and cpus

--- script/main.js
+++ script/main.js
@@ -1,3 +1,67 @@
+const teamSelect = document.querySelector('#team');
+
+teamSelect.addEventListener('change', () => {
+    document.querySelector('#position').disabled = false;
+    let select = document.querySelector('#position');
+    select.innerHTML = '<option value="position" disabled selected>პოზიცია</option>';
+    const teamId = teamSelect.value;
+
+    fetch('https://pcfy.redberryinternship.ge/api/positions') 
+    .then(response => response.json()).then(data => {
+        let positions = data.data;
+        for(let i = 0; i < positions.length; i++) {
+            let option = document.createElement('option');
+            option.value = positions[i].id;
+            option.innerText = positions[i].name;
+            if(positions[i].team_id == teamId) {
+                select.appendChild(option);
+            }
+        }
+    }
+)
+})
+
+
+fetch('https://pcfy.redberryinternship.ge/api/teams')
+    .then(response => response.json()).then(data => {
+        let teams = data.data;
+        let select = document.querySelector('#team');
+        for(let i = 0; i < teams.length; i++) {
+            let option = document.createElement('option');
+            option.value = teams[i].id;
+            option.innerText = teams[i].name;
+            select.appendChild(option);
+        }
+    }
+)
+
+fetch('https://pcfy.redberryinternship.ge/api/brands')
+    .then(response => response.json()).then(data => {
+        let brands = data.data;
+        let select = document.querySelector('#laptop-brand');
+        for(let i = 0; i < brands.length; i++) {
+            let option = document.createElement('option');
+            option.value = brands[i].id;
+            option.innerText = brands[i].name;
+            select.appendChild(option);
+        }
+    }
+)
+
+fetch('https://pcfy.redberryinternship.ge/api/cpus')
+    .then(response => response.json()).then(data => {
+        let cpus = data.data;
+        let select = document.querySelector('#cpu');
+        for(let i = 0; i < cpus.length; i++) {
+            let option = document.createElement('option');
+            option.value = cpus[i].id;
+            option.innerText = cpus[i].name;
+            select.appendChild(option);
+        }
+    }
+)
+
+
 document.querySelector('#date').addEventListener('focus', (event) => {
     event.target.type = 'date';
     let date = new Date();
@@ -10,11 +74,13 @@ document.querySelector('#date').addEventListener('focus', (event) => {
     if(day < 10) {
         day = '0' + day;
     }
-    event.target.value = `${year}-${month}-${day}`;
+    event.target.value = `${year} / ${month} / ${day}`;
 
 })
 
 document.querySelector('#date').addEventListener('blur', (event) => {
-    event.target.type = 'text';
-    event.target.value = '';
+    if(event.target.value === '') {
+        event.target.type = 'text';        
+    }
+
 })
